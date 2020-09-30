248befd16730b3a1417c0512d1d266aa229a616e
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-09-30 17:49:15 -0400

    Fix file name in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout/Layout';
-import Portfolio from '../components/Portfolio/Portfolio';
+import Portfolio from '../components/portfolio/Portfolio';
 import SEO from '../components/SEO';
-
 const IndexPage = () => {
   return (
     <Layout>
